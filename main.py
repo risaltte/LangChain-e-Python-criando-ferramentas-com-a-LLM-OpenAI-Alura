993bf4755061581c6eb03d6a3cd1aee4912ffdbf
risaltte <risaltte@gmail.com>
2026-04-16 11:05:46 -0400

    Definindo a resposta do modelo como um objeto JSON

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ from langchain_openai import ChatOpenAI
 from langchain_groq import ChatGroq
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
+from pydantic import Field, BaseModel
 from dotenv import load_dotenv
+from langchain.globals import set_debug
 import os
+
+# Faz o debug da cadeia -monitora e exibe o fluxo
+# set_debug(True)
 
 # Lê e carrega as variáveis de ambiente do arquivo .env
 load_dotenv()
@@ -12,6 +18,14 @@ load_dotenv()
 api_key_openia = os.getenv("OPENAI_API_KEY")
 api_key_groq = os.getenv("GROQ_API_KEY")
 api_key_gemini = os.getenv("GEMINI_API_KEY")
+
+# Define a estrutura de dados para a resposta esperada do modelo de linguagem usando Pydantic
+class Destino(BaseModel):
+    cidade:str = Field(description="A cidade recomendada para visitar")
+    motivo:str = Field("Motivo pelo qual é interessante visitar essa cidade")
+
+# Configura o parser de saída para interpretar a resposta do modelo de linguagem como um objeto JSON seguindo a estrutura definida pela classe Destino
+parseador = JsonOutputParser(pydantic_object=Destino)
 
 # Configura os modelos de linguagem com as chaves de API e outras opções
 modelo_openia = ChatOpenAI(
@@ -32,31 +46,27 @@ modelo_gemini = ChatGoogleGenerativeAI(
     api_key=api_key_gemini,
 )
 
-numero_dias = 5
-numero_criancas = 2
-atividade = "praia"
-
-# Criação de um modelo/template de prompt usando a classe PromptTemplate
-modelo_de_prompt = PromptTemplate(
+# Configura o prompt para solicitar ao modelo de linguagem uma sugestão de cidade com base em um interesse específico, utilizando o formato de saída definido pelo parser
+prompt_cidade = PromptTemplate(
     template = """
-    Crie um roteiro de viagem de {dias}dias,
-    para uma família com {numero_criancas} crianças,
-    que gostam de {atividade}
-    """
+    Sugira uma cidade dao o meu interresse por {interesse}.
+    {formato_de_saida}
+    """,
+    input_variables=["interesse"],
+    partial_variables={"formato_de_saida": parseador.get_format_instructions()},
+
 )
 
-# Formatação do prompt usando o modelo/template criado, preenchendo os valores das variáveis
-prompt = modelo_de_prompt.format(
-    dias=numero_dias, 
-    numero_criancas=numero_criancas, 
-    atividade=atividade
-)
+# Define o modelo LLM a ser utilizado
+modelo = modelo_groq
 
-# Escolha do modelo de linguagem a ser utilizado 
-modelo = modelo_gemini
+# Configura a cadeia de processamento, encadeando o prompt, o modelo de linguagem e o parser de saída para obter a resposta formatada corretamente
+cadeia = prompt_cidade | modelo | parseador
 
-# Invocação do modelo de linguagem com o prompt formatado e obtenção da resposta
-resposta = modelo.invoke(prompt)
+# Executa a cadeia de processamento, passando o interesse como entrada, e obtém a resposta formatada como um objeto Destino
+resposta = cadeia.invoke({
+    "interesse": "praia"
+})
 
-# Impressão do conteúdo da resposta obtida do modelo de linguagem
-print(resposta.content)
+# Exibe a resposta obtida do modelo de linguagem, que deve conter a cidade recomendada e o motivo para visitá-la
+print(resposta)
