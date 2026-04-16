3f73347a055441b36d695bb162a700607ea4c633
risaltte <risaltte@gmail.com>
2026-04-16 14:02:50 -0400

    Criando uma sequencia de cadeias com LCEL

--- main.py
+++ main.py
@@ -19,13 +19,20 @@ api_key_openia = os.getenv("OPENAI_API_KEY")
 api_key_groq = os.getenv("GROQ_API_KEY")
 api_key_gemini = os.getenv("GEMINI_API_KEY")
 
-# Define a estrutura de dados para a resposta esperada do modelo de linguagem usando Pydantic
+# Define as classes de modelo de dados para a resposta do modelo de linguagem, utilizando o Pydantic para validação e estruturação dos dados
 class Destino(BaseModel):
     cidade:str = Field(description="A cidade recomendada para visitar")
     motivo:str = Field("Motivo pelo qual é interessante visitar essa cidade")
 
-# Configura o parser de saída para interpretar a resposta do modelo de linguagem como um objeto JSON seguindo a estrutura definida pela classe Destino
-parseador = JsonOutputParser(pydantic_object=Destino)
+class Restaurantes(BaseModel):
+    cidade:str = Field(description="A cidade recomendada para visitar")
+    restaurantes:str = Field(description="Restaurantes recomendados na cidade")
+
+
+
+# Configura os parsers de saída para converter a resposta do modelo de linguagem em objetos estruturados, utilizando o formato JSON definido pelas classes Pydantic
+parseador_destino = JsonOutputParser(pydantic_object=Destino)
+parseador_restaurantes = JsonOutputParser(pydantic_object=Restaurantes)
 
 # Configura os modelos de linguagem com as chaves de API e outras opções
 modelo_openia = ChatOpenAI(
@@ -46,22 +53,41 @@ modelo_gemini = ChatGoogleGenerativeAI(
     api_key=api_key_gemini,
 )
 
-# Configura o prompt para solicitar ao modelo de linguagem uma sugestão de cidade com base em um interesse específico, utilizando o formato de saída definido pelo parser
+# 
 prompt_cidade = PromptTemplate(
     template = """
-    Sugira uma cidade dao o meu interresse por {interesse}.
+    Sugira uma cidade dado o meu interresse por {interesse}.
     {formato_de_saida}
     """,
     input_variables=["interesse"],
-    partial_variables={"formato_de_saida": parseador.get_format_instructions()},
-
+    partial_variables={"formato_de_saida": parseador_destino.get_format_instructions()},
 )
+
+prompt_restaurantes = PromptTemplate(
+    template = """
+    Sugira restaurantes populares entre locais em {cidade}.
+    {formato_de_saida}
+    """,
+    partial_variables={"formato_de_saida": parseador_restaurantes.get_format_instructions()},
+)
+
+prompt_cultural = PromptTemplate(
+    template = """
+    Sugira atividades e locais culturais em {cidade}.
+    """
+)
+
 
 # Define o modelo LLM a ser utilizado
 modelo = modelo_groq
 
 # Configura a cadeia de processamento, encadeando o prompt, o modelo de linguagem e o parser de saída para obter a resposta formatada corretamente
-cadeia = prompt_cidade | modelo | parseador
+cadeia_1 = prompt_cidade | modelo | parseador_destino
+cadeia_2 = prompt_restaurantes | modelo | parseador_restaurantes
+cadeia_3 = prompt_cultural | modelo | StrOutputParser()
+
+cadeia = (cadeia_1 | cadeia_2 | cadeia_3)
+
 
 # Executa a cadeia de processamento, passando o interesse como entrada, e obtém a resposta formatada como um objeto Destino
 resposta = cadeia.invoke({
